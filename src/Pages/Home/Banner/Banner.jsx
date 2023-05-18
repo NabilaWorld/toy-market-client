d5c67c203d8a1cb91350016aae15663888b8d4aa
NabilaWorld <mnabila30163@gmail.com>
2023-05-18 10:39:09 +0600

    commit the navbar correction section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,18 +3,19 @@ import imgage from '../../../assets/81gmsyff04L._AC_SX569_.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <div className="card  w-full my-4  bg-base-100 shadow-xl image-full">
-            <figure><img src={imgage} alt="Girls Cooking Toy" /></figure>
-            <div className="card-body mt-28 md:mt-60">
-                <h2 className="font-bold text-center">Shoes!</h2>
-                <p className='text-center'>If a dog chews shoes whose shoes does he choose?</p>
-
-                <div className="text-center">
-                <button className="btn btn-primary">Visit There</button>
+        <div className="relative">
+            <img
+                src={imgage}
+                alt="Banner"
+                className="w-[1220px] my-5 md:ml-16 h-[600px]"
+            />
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                <div className="bg-black bg-opacity-50 p-4 text-white text-center">
+                    <h2 className="text-2xl font-bold">Your Text Goes Here</h2>
+                    <p className="text-lg">Additional Text Description</p>
+                    <button className='btn btn-warning mt-3'>Visit The Page</button>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
